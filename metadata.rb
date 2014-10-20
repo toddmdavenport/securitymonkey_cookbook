92422a6889a425c69a4e9c7a69af21c184f2a1ff
Todd Davenport <tdavenport@neat.com>
2014-10-20 17:33:53 -0400

    version of build-essential

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends "apt"
-depends "build-essential"
+depends "build-essential", ">= 1.4.2"
 depends "git"
 depends "python"
 depends "postgresql"

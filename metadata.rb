41043dd29afeb02469c821c3fe39577a3c09a0a6
Todd Davenport <tdavenport@neat.com>
2014-10-21 10:36:19 -0400

    version bump

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'MIT'
 
 description      'Installs/Configures securitymonkey'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends "apt"
 depends "build-essential", ">= 1.4.2"
